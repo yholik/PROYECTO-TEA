bf96a84b58867b1fdf3ff9627845df1101759c7f
Yamila Holik <shami.nair.holik@gmail.com>
2024-05-28 11:25:19 -0300

    Division de funcion menu y menuTablet
    Se crea un archivo js para menu tablet y dejamos un archivo para menu movil

--- js/menu.js
+++ js/menu.js
@@ -4,12 +4,3 @@ const mainMenuElement = document.getElementById('main_menu');
 toggleMenuElement.addEventListener('click', () => {
     mainMenuElement.classList.toggle('main_menu--show');
 })
-
-/* TABLET */
-window.addEventListener('resize', function () {
-    const element = document.getElementById('banner');
-    if (window.innerWidth > 601) {
-        element.classList.add('banner_tablet');
-        element.classList.remove('banner')
-    };
-})
